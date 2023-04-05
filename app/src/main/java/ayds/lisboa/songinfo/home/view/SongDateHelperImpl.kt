0e7157cfc7db2348bbce8db8d6dcee6c16b95028
RamiroAdrover <ramadrover@gmail.com>
2023-04-05 10:46:31 -0300

    LeapYear, SongDateHelperImpl
    Se implemento la función isLeapYear y se separo en métodos el formateo para cada release date precision

--- app/src/main/java/ayds/lisboa/songinfo/home/view/SongDateHelperImpl.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/SongDateHelperImpl.kt
@@ -1,6 +1,7 @@
 package ayds.lisboa.songinfo.home.view
 
-import app/src/main/java/ayds/lisboa/songinfo/utils/view/LeapYear.kt
+import ayds.lisboa.songinfo.utils.view.LeapYearImpl
+import ayds.lisboa.songinfo.utils.view.LeapYear
 
 interface SongDateHelper {
     fun getFormat(): String
@@ -8,20 +9,20 @@ interface SongDateHelper {
 
 class SongDateHelperImpl(private val releaseDate: String, private val releaseDatePrecision: String): SongDateHelper {
 
-    val date : string = releaseDate.split("-")
+    val date  = releaseDate.split("-")
     override fun getFormat(): String {
-        val result:string
+
+        var result:String = ""
         when (releaseDatePrecision) {
-            "year" ->  result = date.component1() + " " + isLeapYear()
-            "month"-> result = getMonth(date.component2()) + ", " +  date.component1()
-            "day" -> result = date.component3() + "/" + date.component2() + "/" + date.component1()
+            "year" ->  result = getYearFormat()
+            "month"-> result =  getMonthFormat()
+            "day" -> result =  getDayFormat()
         }
         return result
     }
-    private fun getMonth(private val Month: String):String
+    private fun getMonth(Month: String):String
     {
-
-        val result:string
+        var result:String =""
         when(Month) {
             "1"->result = "January"
             "2"->result = "February"
@@ -36,7 +37,19 @@ class SongDateHelperImpl(private val releaseDate: String, private val releaseDat
             "11"->result = "November"
             "12"->result = "December"
         }
-
         return result
+    }
+
+    private fun getYearFormat():String{
+        var leapYear:LeapYear = LeapYearImpl(date.component1().toInt())
+        return date.component1() + " " + leapYear.isLeapYear()
+    }
+
+    private fun getMonthFormat():String{
+        return getMonth(date.component2()) + ", " +  date.component1()
+    }
+
+    private fun getDayFormat():String{
+        return date.component3() + "/" + date.component2() + "/" + date.component1()
     }
 }
--- app/src/main/java/ayds/lisboa/songinfo/utils/view/LeapYear.kt
+++ app/src/main/java/ayds/lisboa/songinfo/utils/view/LeapYear.kt
@@ -2,12 +2,11 @@ package ayds.lisboa.songinfo.utils.view
 
 
 interface LeapYear {
-
-    fun isLeapYear(): Boolean
+    fun isLeapYear(): String
 }
 
 class LeapYearImpl(private val year: Int): LeapYear {
-    override fun isLeapYear(): Boolean {
-        TODO("Not yet implemented")
+    override fun isLeapYear(): String {
+        return if((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) "(leap year)" else "(not a leap year)"
     }
 }

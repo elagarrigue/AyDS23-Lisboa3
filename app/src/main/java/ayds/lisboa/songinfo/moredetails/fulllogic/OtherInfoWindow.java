d5a1d33444076af232e09b4b48279c7832909434
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2023-04-16 15:09:59 -0300

    Update OtherInfoWindow.java
    1. Se agregan constantes.
    2. Se mejora la indentación.
    3. Se mueven las funciones create() a funciones privadas.

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.java
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.java
@@ -3,9 +3,7 @@ package ayds.lisboa.songinfo.moredetails.fulllogic;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.Html;
-import android.text.util.Linkify;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,133 +13,126 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ayds.lisboa.songinfo.R;
-import ayds.lisboa.songinfo.home.model.entities.Song;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
 
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-
-
 public class OtherInfoWindow extends AppCompatActivity {
-
-  public final static String ARTIST_NAME_EXTRA = "artistName";
-
-  private TextView textPane2;
-  //private JPanel imagePanel;
- // private JLabel posterImageLabel;
+  private final static String ARTIST_NAME_EXTRA = "artistName";
+  private final static String TAG = "tag";
+  private TextView view;
+  private final static int layout = R.layout.activity_other_info;
+  private final static int textPaneID = R.id.textPane2;
+  private final static String retrofitBaseURL = "https://ws.audioscrobbler.com/2.0/";
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    setContentView(R.layout.activity_other_info);
-
-    textPane2 = findViewById(R.id.textPane2);
-
-
-    open(getIntent().getStringExtra("artistName"));
+    setContentView(layout);
+    view = findViewById(textPaneID);
+    open(getIntent().getStringExtra(ARTIST_NAME_EXTRA));
   }
 
-  public void getARtistInfo(String artistName) {
+  public void getArtistInfo(String artistName) {
+    Retrofit retrofit = createRetrofit();
+    LastFMAPI lastFMAPI = createLastFMAPI(retrofit);
 
-    // create
+    Log.e(TAG,ARTIST_NAME_EXTRA + " " + artistName);
+
+    new Thread(() -> {
+      String text = DataBase.getInfo(dataBase, artistName);
+      if (text != null) { // exists in db
+
+        text = "[*]" + text;
+      } else { // get from service
+        Response<String> callResponse;
+        try {
+          callResponse = lastFMAPI.getArtistInfo(artistName).execute();
+
+          Log.e(TAG,"JSON " + callResponse.body());
+
+          Gson gson = new Gson();
+          JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
+          JsonObject artist = jobj.get("artist").getAsJsonObject();
+          JsonObject bio = artist.get("bio").getAsJsonObject();
+          JsonElement extract = bio.get("content");
+          JsonElement url = artist.get("url");
+
+
+          if (extract == null) {
+            text = "No Results";
+          } else {
+            text = extract.getAsString().replace("\\n", "\n");
+
+            text = textToHtml(text, artistName);
+
+
+            // save to DB  <o/
+
+            DataBase.saveArtist(dataBase, artistName, text);
+          }
+
+
+          final String urlString = url.getAsString();
+          findViewById(R.id.openUrlButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(Intent.ACTION_VIEW);
+              intent.setData(Uri.parse(urlString));
+              startActivity(intent);
+            }
+          });
+
+        } catch (IOException e1) {
+          Log.e(TAG, "Error " + e1);
+          e1.printStackTrace();
+        }
+        }
+
+
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png";
+
+        Log.e(TAG,"Get Image from " + imageUrl);
+
+
+
+        final String finalText = text;
+
+        runOnUiThread( () -> {
+          Picasso.get().load(imageUrl).into((ImageView) findViewById(R.id.imageView));
+
+
+          view.setText(Html.fromHtml( finalText));
+
+
+        });
+
+
+
+      }).start();
+
+  }
+
+  private Retrofit createRetrofit() {
     Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://ws.audioscrobbler.com/2.0/")
+            .baseUrl(retrofitBaseURL)
             .addConverterFactory(ScalarsConverterFactory.create())
             .build();
 
-    LastFMAPI lastFMAPI = retrofit.create(LastFMAPI.class);
+    return retrofit;
+  }
 
-    Log.e("TAG","artistName " + artistName);
-
-        new Thread(new Runnable() {
-          @Override
-          public void run() {
-
-            String text = DataBase.getInfo(dataBase, artistName);
-
-
-            if (text != null) { // exists in db
-
-              text = "[*]" + text;
-            } else { // get from service
-              Response<String> callResponse;
-              try {
-                callResponse = lastFMAPI.getArtistInfo(artistName).execute();
-
-                Log.e("TAG","JSON " + callResponse.body());
-
-                Gson gson = new Gson();
-                JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
-                JsonObject artist = jobj.get("artist").getAsJsonObject();
-                JsonObject bio = artist.get("bio").getAsJsonObject();
-                JsonElement extract = bio.get("content");
-                JsonElement url = artist.get("url");
-
-
-                if (extract == null) {
-                  text = "No Results";
-                } else {
-                  text = extract.getAsString().replace("\\n", "\n");
-
-                  text = textToHtml(text, artistName);
-
-
-                  // save to DB  <o/
-
-                  DataBase.saveArtist(dataBase, artistName, text);
-                }
-
-
-                final String urlString = url.getAsString();
-                findViewById(R.id.openUrlButton).setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(urlString));
-                    startActivity(intent);
-                  }
-                });
-
-              } catch (IOException e1) {
-                Log.e("TAG", "Error " + e1);
-                e1.printStackTrace();
-              }
-            }
-
-
-            String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png";
-
-            Log.e("TAG","Get Image from " + imageUrl);
-
-
-
-            final String finalText = text;
-
-            runOnUiThread( () -> {
-              Picasso.get().load(imageUrl).into((ImageView) findViewById(R.id.imageView));
-
-
-              textPane2.setText(Html.fromHtml( finalText));
-
-
-            });
-
-
-
-          }
-        }).start();
-
+  private LastFMAPI createLastFMAPI(Retrofit retrofit) {
+    Class<LastFMAPI> type = LastFMAPI.class;
+    return retrofit.create(type);
   }
 
   private DataBase dataBase = null;
@@ -157,7 +148,7 @@ public class OtherInfoWindow extends AppCompatActivity {
     Log.e("TAG", ""+ DataBase.getInfo(dataBase,"test"));
     Log.e("TAG",""+ DataBase.getInfo(dataBase,"nada"));
 
-    getARtistInfo(artist);
+    getArtistInfo(artist);
   }
 
   public static String textToHtml(String text, String term) {

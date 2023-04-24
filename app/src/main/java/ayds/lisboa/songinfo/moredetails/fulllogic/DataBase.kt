3a216033a085436318d378f5009cd38d4927bf06
RamiroAdrover <ramadrover@gmail.com>
2023-04-24 16:09:00 -0300

    Update Databa.kt
    - Se elimino la función publica que no se utilizaba y la función privada que la complementaba.
    - Se eliminaron los espacios entre funciones
    - Se eliminaron imports y variables que no se utilizaban
    - Se cambiaron los nombres a constantes para mantener consistencia
    - Se cambio items a un atributo de la clase

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -6,66 +6,30 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
-import java.sql.Statement
 
-private const val dbUrl = "jdbc:sqlite:./dictionary.db"
-private const val artistQuery = "select * from artists"
 private const val ID = "id"
 private const val ARTIST = "artist"
 private const val INFO = "info"
 private const val SOURCE = "source"
 private const val TABLE_ARTISTS = "artists"
-private const val resultSetSortOrder = "artist DESC"
-private const val artistColumn = "artist  = ?"
-private val items: MutableList<String> = ArrayList()
-private const val creationQuery = "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
+private const val RESULT_SET_ORDER = "artist DESC"
+private const val ARTIST_COLUMN = "artist  = ?"
+private const val CREATION_QUERY = "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
 
 class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
-
+    private val items: MutableList<String> = ArrayList()
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
-            creationQuery
+            CREATION_QUERY
         )
         Log.i("DB", "DB created")
     }
-
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
-
-    fun testDB() {
-        var connection: Connection? = null
-        try {
-            connection = DriverManager.getConnection(dbUrl)
-            val statement = connection.createStatement()
-            statement.queryTimeout = 30
-            resultSetValues(statement)
-        } catch (e: SQLException) {
-            System.err.println(e.message)
-        } finally {
-            try {
-                connection?.close()
-            } catch (e: SQLException) {
-                System.err.println(e)
-            }
-        }
-    }
-    private fun resultSetValues(statement: Statement){
-        val rs = statement.executeQuery(artistQuery)
-        while (rs.next()) {
-            println("id = " + rs.getInt(ID))
-            println("artist = " + rs.getString(ARTIST))
-            println("info = " + rs.getString(INFO))
-            println("source = " + rs.getString(SOURCE))
-        }
-    }
     fun saveArtist(artist: String, info: String) {
         val writableDatabase = this.writableDatabase
         val artistValues = getArtistValues(artist, info)
         writableDatabase.insert(TABLE_ARTISTS, null, artistValues)
     }
-
     private fun getArtistValues(artist: String, info: String): ContentValues {
         val values = ContentValues()
         values.put(ARTIST, artist)
@@ -73,25 +37,22 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
         values.put(SOURCE, 1)
         return values
     }
-
     fun getInfo(artist: String): String {
         val readableDatabase = this.readableDatabase
-
         val databaseColumns = arrayOf(
             ID,
             ARTIST,
             INFO
         )
-
         val artistValues = arrayOf(artist)
         val cursor = readableDatabase.query(
             TABLE_ARTISTS,
             databaseColumns,
-            artistColumn,
+            ARTIST_COLUMN,
             artistValues,
             null,
             null,
-            resultSetSortOrder
+            RESULT_SET_ORDER
         )
         insertInItems(cursor)
         cursor.close()
@@ -101,7 +62,6 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
             items[0]
         }
     }
-
     private fun insertInItems(cursor: Cursor) {
         var info: String
         while (cursor.moveToNext()) {

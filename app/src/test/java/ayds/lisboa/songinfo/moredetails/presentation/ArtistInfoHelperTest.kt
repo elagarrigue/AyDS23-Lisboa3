4330b0f5287026968bdc948e7cedbcca0d3086a7
RamiroAdrover <ramadrover@gmail.com>
2023-05-15 18:09:37 -0300

    Update ArtistInfoHelperTest.kt

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistInfoHelperTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistInfoHelperTest.kt
@@ -1,9 +1,68 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
+import ayds.lisboa.songinfo.moredetails.domain.entities.ArtistInfo
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class ArtistInfoHelperTest {
+
+    private val htmlHelper = mockk<HtmlHelper>()
+    private val artistInfoHelper by lazy {ArtistInfoHelperImpl(htmlHelper)}
+
+    @Test
+    fun `given an artist name and an artist info it should return the formatted text`() {
+        // Arrange
+        val artistName = ""
+        val artistInfo: ArtistInfo = ArtistInfo.LastFmArtistInfo(
+            "bioContent",
+            "url",
+            false
+        )
+        every { htmlHelper.getHtmlText(any(), any()) } returns "Formatted bio"
+        val result = artistInfoHelper.getArtistInfoText(artistName, artistInfo)
+        assertEquals("Formatted bio", result)
+    }
+
+    @Test
+    fun `given a non last fm artist info return not found`(){
+        val artistName=""
+        val artistInfo: ArtistInfo = mockk()
+        val result = artistInfoHelper.getArtistInfoText(artistName,artistInfo)
+        val expected = "Artist info not found."
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given an artist info it should return the url`() {
+        val artistInfo = ArtistInfo.LastFmArtistInfo("bioContent", "url", false)
+        val expectedUrl = "url"
+
+        val result = artistInfoHelper.getArtistInfoUrl(artistInfo)
+
+        assertEquals(expectedUrl, result)
+    }
+
+    @Test
+    fun `given an artist info with invalid url it should return the no url found message`() {
+        val artistInfo = ArtistInfo.LastFmArtistInfo("bioContent", "", false)
+        val expectedUrl = ""
+
+        val result = artistInfoHelper.getArtistInfoUrl(artistInfo)
+
+        assertEquals(expectedUrl, result)
+    }
+
+    @Test
+    fun `given an artist info that is not LastFmArtistInfo it should return the no url found message`() {
+        val artistInfo = ArtistInfo.EmptyArtistInfo
+        val expectedUrl = "Artist info url not found."
+        val result = artistInfoHelper.getArtistInfoUrl(artistInfo)
+
+        assertEquals(expectedUrl, result)
+    }
+
+
 
 }

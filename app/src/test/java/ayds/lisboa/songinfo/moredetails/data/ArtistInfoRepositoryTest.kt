ee3f92f5658f285d65e2b6f5a47bc85459898af7
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2023-05-15 16:06:32 -0300

    Template tests
    Co-Authored-By: Ramiro Adrover <72950206+RamiroAdrover@users.noreply.github.com>
    Co-Authored-By: Nahuel Maika <43748108+NahuelMaika@users.noreply.github.com>
    Co-Authored-By: Mathis MAZOYER <78586669+MathisMAZOYER@users.noreply.github.com>

---
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/data/ArtistInfoRepositoryTest.kt
@@ -0,0 +1,9 @@
+package ayds.lisboa.songinfo.moredetails.data
+
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class ArtistInfoRepositoryTest {
+
+}
---
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -0,0 +1,9 @@
+package ayds.lisboa.songinfo.moredetails.presentation
+
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class OtherInfoPresenterTest {
+
+}

6520e67c8d695fdc5567113bb9b1913cea1cbe77
Nahuel Maika <nahuelmaika@gmail.com>
2023-05-15 19:15:41 -0300

    Update OtherInfoPresenterTest.kt

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -1,9 +1,36 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
+import ayds.lisboa.songinfo.home.controller.HomeControllerImpl
+import ayds.lisboa.songinfo.home.model.entities.Song
+import ayds.lisboa.songinfo.home.view.HomeUiEvent
+import ayds.lisboa.songinfo.home.view.HomeUiState
+import ayds.lisboa.songinfo.moredetails.dependencyinyector.MoreDetailsInjector
+import ayds.lisboa.songinfo.moredetails.domain.entities.ArtistInfo
+import ayds.lisboa.songinfo.moredetails.domain.repository.ArtistInfoRepository
+import ayds.observer.Observable
+import ayds.observer.Subject
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
-internal class OtherInfoPresenterTest {
+class OtherInfoPresenterTest {
+    private val moreDetailsInjector =MoreDetailsInjector
+    private val otherInfoView: OtherInfoView = mockk()
+    private lateinit var otherInfoPresenter: OtherInfoPresenter
+
+    @Before
+    fun setup() {
+        moreDetailsInjector.init(otherInfoView)
+        otherInfoPresenter = moreDetailsInjector.getPresenter()
+    }
+
+    @Test
+    fun `on fetch should notify view with uiState`() {
+
+        verify { otherInfoPresenter.fetch("artist") }
+    }
 
 }

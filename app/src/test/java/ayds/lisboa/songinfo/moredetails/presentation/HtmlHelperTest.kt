c975c1d32cbb52a5c5e27afca5b47ae6c63fe797
Mathis MAZOYER <78586669+MathisMAZOYER@users.noreply.github.com>
2023-05-15 18:28:42 -0300

    Update HtmlHelperTest.kt
    Se agrego el test de la clase HtmlHelper

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/HtmlHelperTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/HtmlHelperTest.kt
@@ -1,9 +1,23 @@
-package ayds.lisboa.songinfo.moredetails.presentation
-
-import io.mockk.mockk
+import ayds.lisboa.songinfo.moredetails.presentation.HtmlHelper
+import ayds.lisboa.songinfo.moredetails.presentation.HtmlHelperImpl
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-internal class HtmlHelperTest {
+class HtmlHelperImplTest {
 
+    private val htmlHelper: HtmlHelper by lazy {
+        HtmlHelperImpl()
+    }
+    @Test
+    fun `getHtmlText should return the correct HTML text`() {
+
+        val text = "Esto es una frase de testing"
+        val term = "frase"
+
+        val expectedText = """<html><div width=400><font face="arial">Esto es una frase de testing</font></div></html>""".trimIndent()
+
+        val result = htmlHelper.getHtmlText(text, term)
+
+        assertEquals(expectedText, result)
+    }
 }

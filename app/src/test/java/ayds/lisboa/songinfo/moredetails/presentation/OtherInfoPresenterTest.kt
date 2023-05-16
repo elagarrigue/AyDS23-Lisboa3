91c87b74d1fa07cb34286d36a2ef277df834b399
Nahuel Maika <nahuelmaika@gmail.com>
2023-05-15 22:13:48 -0300

    Update OtherInfoPresenterTest.kt

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -1,27 +1,21 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
-import ayds.lisboa.songinfo.home.controller.HomeControllerImpl
-import ayds.lisboa.songinfo.home.model.entities.Song
-import ayds.lisboa.songinfo.home.view.HomeUiEvent
-import ayds.lisboa.songinfo.home.view.HomeUiState
+
 import ayds.lisboa.songinfo.moredetails.dependencyinyector.MoreDetailsInjector
-import ayds.lisboa.songinfo.moredetails.domain.entities.ArtistInfo
 import ayds.lisboa.songinfo.moredetails.domain.repository.ArtistInfoRepository
-import ayds.observer.Observable
-import ayds.observer.Subject
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class OtherInfoPresenterTest {
     private val moreDetailsInjector =MoreDetailsInjector
     private val otherInfoView: OtherInfoView = mockk()
-    private val artistInfoRepository: ArtistInfoRepository = mockk(relaxUnitFun = true)
-    private val artistInfoHelper: ArtistInfoHelper= mockk(relaxUnitFun = true)
+    private val artistInfoRepository: ArtistInfoRepository = mockk(relaxed = true)
+    private val artistInfoHelper: ArtistInfoHelper= mockk(relaxed = true)
+
+
 
     private val otherInfoPresenter = spyk(OtherInfoPresenterImpl(artistInfoRepository,artistInfoHelper))
 
@@ -34,7 +28,12 @@ class OtherInfoPresenterTest {
     @Test
     fun `on fetch should notify view with uiState`() {
         otherInfoPresenter.fetch("artist")
-        verify { otherInfoPresenter.fetch("artist") }
+
+        verify { val artist =
+            artistInfoRepository.getArtistInfo("artist")
+            artistInfoHelper.getArtistInfoText("artist",artist)
+            artistInfoHelper.getArtistInfoUrl(artist)}
+
     }
 
 }

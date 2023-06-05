2cc2450e7161b941010d02996983d4737c2f7007
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2023-06-05 16:43:05 -0300

    Detalle en HtmlHelperTest
    Co-Authored-By: Mathis MAZOYER <78586669+MathisMAZOYER@users.noreply.github.com>
    Co-Authored-By: Nahuel Maika <43748108+NahuelMaika@users.noreply.github.com>
    Co-Authored-By: Ramiro Adrover <72950206+RamiroAdrover@users.noreply.github.com>

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/HtmlHelperTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/HtmlHelperTest.kt
@@ -1,21 +1,18 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class HtmlHelperTest {
 
-    private val htmlHelper = mockk<HtmlHelper>()
+    private val htmlHelper: HtmlHelper by lazy { HtmlHelperImpl() }
 
     @Test
     fun `getHtmlText should return the correct HTML text`() {
         val text = "Hello world Lisbon 3"
         val term = "world"
-        val expectedHtmlText = "<html><div width=400><font face=\"arial\">Hello <b>WORLD</b> Lisbon 3</font></div></html>"
 
-        every { htmlHelper.getHtmlText(text, term) } returns expectedHtmlText
+        val expectedHtmlText = "<html><div width=400><font face=\"arial\">Hello <b>WORLD</b> Lisbon 3</font></div></html>"
         val result = htmlHelper.getHtmlText(text, term)
 
         assertEquals(expectedHtmlText, result)
